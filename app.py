da26b13892fe9e7358d3fd68a60764bda64c4c7e
Frank Wilson <franklinhw@gmail.com>
2025-03-30 15:33:45 +0100

    Added jinja templates

--- app.py
+++ app.py
@@ -6,5 +6,15 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
+@app.route('/hello/')
+@app.route('/hello/<name>')
+def hello(name=None):
+    return render_template('hello.html', person=name)
+
+@app.route('/users/')
+def users(user_list=None):
+    user_list = ['user 1', 'user 2', 'user 3']
+    return render_template('list.html', users=user_list)
+
 if __name__ == "__main__":
     app.run(debug=True)
